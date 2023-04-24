2c57bad47163d0913593073bb33d93c024e5f082
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-24 03:02:22 +0100

    feat: Setup mongodb adapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,3 +1,5 @@
+import clientPromise from '@/lib/mongodb';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import NextAuth from 'next-auth';
 
 import GoogleProvider from 'next-auth/providers/google';
@@ -12,4 +14,5 @@ export default NextAuth({
     }),
     // Passwordless / email sign in
   ],
+  adapter: MongoDBAdapter(clientPromise),
 });
