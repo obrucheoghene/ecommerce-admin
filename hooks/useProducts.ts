f321c9e770bfb6ac50ad7f064ca193ad36e5515c
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-26 13:39:53 +0100

    fix: Fix useProducts hooks url undefined

--- hooks/useProducts.ts
+++ hooks/useProducts.ts
@@ -1,7 +1,7 @@
 import fetcher from '@/lib/fetcher';
 import useSWR from 'swr';
 const useProducts = (productId?: string) => {
-  const uri = `api/products/${productId && productId}`;
+  const uri = `api/products/${productId ? productId : ''}`;
   const { data, isLoading, error, mutate } = useSWR(uri, fetcher);
 
   return { data, isLoading, error, mutate };
