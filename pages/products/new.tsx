7b358c05ad31fda95cd72d0860c409f74559f3c0
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-26 12:43:31 +0100

    feat: Add new product page to add product

--- pages/products/new.tsx
+++ pages/products/new.tsx
@@ -1,21 +1,53 @@
 import Layout from '@/components/Layout';
-import React from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import React, { useCallback, useState } from 'react';
 
 const NewProduct = () => {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = useCallback(async () => {
+    const data = { name, description, price };
+    try {
+      await axios.post('/api/products', data);
+      router.push('/products');
+    } catch (error) {
+      console.log(error);
+    }
+  }, [name, description, price, router]);
+
   return (
     <Layout>
       <h1> New Product</h1>
-
       <label htmlFor="">Product Name</label>
-      <input type="text" placeholder="Product name" />
+      <input
+        type="text"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        placeholder="Product name"
+      />
 
       <label htmlFor="">Description</label>
-      <textarea placeholder="Description"></textarea>
+      <textarea
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+        placeholder="Description"
+      />
 
       <label htmlFor="">Price (in USD)</label>
-      <input type="text" placeholder="Price" />
+      <input
+        type="number"
+        value={price}
+        onChange={(event) => setPrice(event.target.value)}
+        placeholder="Price"
+      />
 
-      <button className="btn-primary">Save</button>
+      <button onClick={handleSubmit} className="btn-primary">
+        Save
+      </button>
     </Layout>
   );
 };
