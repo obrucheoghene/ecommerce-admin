7b0ad818660c37e4c8d676027433fc53d5f5a7ee
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-26 13:40:44 +0100

    feat: Show fetched products in table

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,14 +1,41 @@
 import Layout from '@/components/Layout';
+import useProducts from '@/hooks/useProducts';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
+import { AiFillEdit } from 'react-icons/ai';
 
 const Products = () => {
+  const { data: fetchedProducts, isLoading, error, mutate } = useProducts();
+
+  console.log(fetchedProducts);
   return (
     <Layout>
       <Link href="/products/new" className="btn-primary">
         Add new products
       </Link>
+
+      <table className=" basic mt-2">
+        <thead>
+          <tr>
+            <td>Product name</td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          {fetchedProducts?.map((product: Record<string, any>) => (
+            <tr key={product._id}>
+              <td>{product.name}</td>
+              <td>
+                <Link href={`/api/products/${product._id}`}>
+                  <AiFillEdit />
+                  <span>Edit</span>
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Layout>
   );
 };
