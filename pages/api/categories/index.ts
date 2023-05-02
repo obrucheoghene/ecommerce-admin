5bd18acd6686d97cfc444bd273188f7fde5d86e8
wilfredcloud <wilfredcloudspace@gmail.com>
2023-05-02 14:25:21 +0100

    feat(api): Add create category api

---
+++ pages/api/categories/index.ts
@@ -0,0 +1,31 @@
+import { Category, createCategory, getCategoryByName } from '@/models/category';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req;
+
+  if (method !== 'POST' && method !== 'GET') {
+    return res.status(400).end();
+  }
+
+  if (method === 'POST') {
+    const { name } = req.body;
+    if (!name) {
+      return res.status(400).end();
+    }
+    try {
+      const existingCategory = await getCategoryByName(name);
+      if (existingCategory) {
+        return res.status(409).json({ error: 'Category already exist' });
+      }
+      const category = await Category.create({ name });
+      return res.status(200).json(category);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).end();
+    }
+  }
+}
