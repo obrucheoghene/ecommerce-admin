4250c86973c551175f303a56f00b551d6eeccb2d
wilfredcloud <wilfredcloudspace@gmail.com>
2023-05-02 15:23:30 +0100

    feat: Add parent to category model

--- models/category.ts
+++ models/category.ts
@@ -1,14 +1,18 @@
+import { ObjectId } from 'mongodb';
 import { Schema, models, model } from 'mongoose';
 
 const CategorySchema = new Schema({
   name: { type: String, require: true },
+  parent: { type: ObjectId, ref: 'Category' },
 });
 
 export const Category = models?.Category || model('Category', CategorySchema);
 
 // Actions
-export const getCategories = () => Category.find();
-export const getCategoryById = (_id: string) => Category.findById(_id);
-export const getCategoryByName = (name: string) => Category.findOne({ name });
 export const createCategory = (data: Record<string, any>) =>
   Category.create(data);
+export const getCategories = () => Category.find().populate('parent');
+export const getCategoryById = (_id: string) => Category.findById(_id);
+export const getCategoryByName = (name: string) => Category.findOne({ name });
+export const getCategoriesByParent = (parent: string) =>
+  Category.find({ parent });
