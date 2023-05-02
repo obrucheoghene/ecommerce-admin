2fa4101c10a461ea405061cfcf68481c74db801a
wilfredcloud <wilfredcloudspace@gmail.com>
2023-05-02 12:40:33 +0100

    test: Fix error in product index test

--- __test__/pages/products/index.test.tsx
+++ __test__/pages/products/index.test.tsx
@@ -1,26 +1,52 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { SessionProvider, useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import Products from '@/pages/products';
 
 // Mock the useSession hook
-jest.mock('next-auth/react', () => ({
-  useSession: jest.fn(),
-}));
+jest.mock('next-auth/react', () => {
+  const actualModule = jest.requireActual('next-auth/react');
+  const mockSession = {
+    expires: '2023-05-31T02:13:48.232Z',
+    user: {
+      name: 'Wilfred Obruche',
+      image:
+        'https://lh3.googleusercontent.com/a/AGNmyxYSztcVPTQeXQXWu65DHIJ1q6nz6EHbNmqAHWhJ=s96-c',
+      email: 'wilfredcloudspace@gmail.com',
+    },
+  };
+  return {
+    __esModule: true,
+    ...actualModule,
+    useSession: jest.fn(() => {
+      return { data: mockSession, status: 'authenticated' };
+    }),
+  };
+});
+
+jest.mock('next/router', () => {
+  const actualModule = jest.requireActual('next/router');
+
+  const mockRouter = {
+    pathname: '/products',
+    query: { productId: '644f2c9709ad71b7adf59dac' },
+  };
+
+  return {
+    __esModule: true,
+    ...actualModule,
+    useRouter: jest.fn(() => {
+      return { ...mockRouter };
+    }),
+  };
+});
 
 describe('The Product Index Page', () => {
-  it('Renders the Product Index Page if User is Authenticated', () => {
-    // Mock the useSession hook to return a mocked session object
-    (useSession as jest.Mock).mockReturnValueOnce([
-      { user: { name: 'John Doe' } },
-      false,
-    ]);
-
-    // Render the component wrapped in the SessionProvider
+  it('Renders the Product Index Page if User is Authenticated', async () => {
     render(<Products />);
 
-    const button = screen.getByRole('button');
+    const text = screen.getByText(/Add new products/i);
 
-    expect(button).toHaveTextContent('Add new Products');
+    expect(text).toBeInTheDocument();
   });
 });
