821b6631395fc46503c59a1d094f88ae573d59c0
wilfredcloud <wilfredcloudspace@gmail.com>
2023-05-04 14:31:20 +0100

    feat: Add actions to product model

--- models/product.ts
+++ models/product.ts
@@ -10,3 +10,14 @@ const ProductSchema = new Schema({
 });
 
 export const Product = models?.Product || model('Product', ProductSchema);
+
+export const createProduct = (data: Record<string, any>) =>
+  Product.create(data);
+export const getProducts = () => Product.find();
+export const getProductById = (_id: string) => Product.findById(_id);
+export const getProductByCategory = (category: string) =>
+  Product.find({ category: category });
+export const updateProductById = (_id: string, data: Record<string, any>) =>
+  Product.findByIdAndUpdate(_id, data);
+export const deleteProductById = (_id: string) =>
+  Product.findByIdAndDelete(_id);
