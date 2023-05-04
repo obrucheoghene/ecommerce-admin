455feafbaec2e0e9736f47e53bd0267ac9214b8d
wilfredcloud <wilfredcloudspace@gmail.com>
2023-05-04 14:08:23 +0100

    feat: Add category to product model

--- models/product.ts
+++ models/product.ts
@@ -1,8 +1,10 @@
+import { ObjectId } from 'mongodb';
 import { Schema, model, models } from 'mongoose';
 
 const ProductSchema = new Schema({
   name: { type: String, required: true },
   description: String,
+  category: { type: ObjectId, ref: 'Category' },
   price: { type: Number, required: true },
   images: { type: [String] },
 });
