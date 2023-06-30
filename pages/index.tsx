55efa22df2e17eeada5584f70321d717579d3cdf
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-30 09:49:19 +0100

    refactor: Remove top content

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,22 +6,7 @@ export default function Home() {
   const { data: session } = useSession();
   return (
     <Layout>
-      <div className=" text-blue-900 flex justify-between items-center">
-        <h2>
-          {' '}
-          Hello <b>{session?.user?.name}</b>
-        </h2>
-        <div className=" flex bg-gray-300 text-black items-center rounded-lg pr-2 overflow-hidden">
-          <Image
-            src={'/images/defaultAvatar.png'}
-            width={40}
-            height={40}
-            alt="User image"
-            className=""
-          />
-          <p>{session?.user?.name}</p>
-        </div>
-      </div>
+      
     </Layout>
   );
 }
